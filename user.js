5f1207cc818d3fa065ec101a3cd50ecdba7b7956
Kelly Selden <kellyselden@gmail.com>
2024-11-15 13:31:20 -0800

    fixes

--- user.js
+++ user.js
@@ -1,82 +1,127 @@
 // ==UserScript==
 // @name         Jenkins Build Status Favicon
 // @namespace    https://github.com/kellyselden
-// @version      2
+// @version      3
 // @description  Monitor builds using tab icons
 // @updateURL    https://raw.githubusercontent.com/kellyselden/jenkins-build-status-favicon/main/meta.js
 // @downloadURL  https://raw.githubusercontent.com/kellyselden/jenkins-build-status-favicon/main/user.js
 // @author       Kelly Selden
 // @license      MIT
 // @supportURL   https://github.com/kellyselden/jenkins-build-status-favicon
-// @match        http*://*jenkins*/*
+// @match        http*://*jenkins*/job/*
 // ==/UserScript==
+'use strict';
 
-(() => {
-  const icons = {
-    'In progress': '🔵',
-    'Success': '🟢',
-    'Failed': '🔴',
-    'Aborted': '⚪️',
-  };
+const icons = {
+  'In progress': '🔵',
+  'Success': '🟢',
+  'Failed': '🔴',
+  'Aborted': '⚪️',
+};
 
-  function updateFavicon() {
-    let statusText;
+const statusIconClass = 'a.build-status-link';
 
-    let status = document.querySelector('.jenkins-build-caption svg');
+function getFavicon() {
+  return document.head.querySelector('link[rel="shortcut icon"]');
+}
 
-    if (status) {
-      statusText = status.getAttribute('tooltip');
-    }
+function replaceFavicon(favicon) {
+  if (favicon) {
+    favicon.href = '/favicon.ico';
+  }
+}
 
-    let favicon = document.head.querySelector('link[rel="shortcut icon"]');
+function updateFavicon(status) {
+  let favicon = getFavicon();
 
-    if (!statusText) {
-      if (favicon) {
-        favicon.href = '/favicon.ico';
-      }
+  let statusText = status.getAttribute('tooltip');
 
-      return;
-    }
+  if (!statusText) {
+    replaceFavicon(favicon);
 
-    // Sometimes the favicon gets stuck on the Jenkins logo,
-    // even though the element is set to the status.
-    // Doing this seems to jump start it into working.
-    if (favicon) {
-      document.head.removeChild(favicon);
-
-      favicon = null;
-    }
-
-    if (!favicon) {
-      favicon = document.createElement('link');
-
-      favicon.rel = 'shortcut icon';
-
-      document.head.appendChild(favicon);
-    }
-
-    let svg = document.createElement('svg');
-
-    svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
-
-    let icon = document.createElement('text');
-
-    icon.setAttribute('font-size', '13');
-    icon.setAttribute('y', '13');
-
-    icon.textContent = icons[statusText] ?? '❓';
-
-    svg.appendChild(icon);
-
-    favicon.href = `data:image/svg+xml,${svg.outerHTML}`;
+    return;
   }
 
-  updateFavicon();
+  let iconText = icons[statusText.replace(' > Console Output', '')];
 
-  new MutationObserver(updateFavicon).observe(document.body, {
-    subtree: true,
-    childList: true,
-    attributeFilter: ['tooltip'],
-    characterData: true,
-  });
-})();
+  if (!iconText) {
+    iconText = '❓';
+  }
+
+  // Sometimes the favicon gets stuck on the Jenkins logo,
+  // even though the element is set to the status.
+  // Doing this seems to jump start it into working.
+  if (favicon) {
+    document.head.removeChild(favicon);
+
+    favicon = null;
+  }
+
+  if (!favicon) {
+    favicon = document.createElement('link');
+
+    favicon.rel = 'shortcut icon';
+
+    document.head.appendChild(favicon);
+  }
+
+  let svg = document.createElement('svg');
+
+  svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+
+  let icon = document.createElement('text');
+
+  icon.setAttribute('font-size', '13');
+  icon.setAttribute('y', '13');
+
+  icon.textContent = iconText;
+
+  svg.appendChild(icon);
+
+  favicon.href = `data:image/svg+xml,${svg.outerHTML}`;
+}
+
+let container = document.querySelector('#buildHistory tbody');
+
+if (!container) {
+  return;
+}
+
+let status = container.querySelector(statusIconClass);
+
+if (status) {
+  updateFavicon(status);
+}
+
+function find(node, query) {
+  if (node.matches?.(query)) {
+    return node;
+  } else {
+    return node.querySelector?.(query);
+  }
+}
+
+new MutationObserver(mutationsList => {
+  for (let mutation of mutationsList) {
+    if (mutation.type === 'childList') {
+      for (let node of mutation.addedNodes) {
+        let status = find(node, statusIconClass);
+
+        if (status) {
+          updateFavicon(status);
+        }
+      }
+
+      for (let node of mutation.removedNodes) {
+        let status = find(node, statusIconClass);
+
+        if (status) {
+          replaceFavicon();
+        }
+      }
+    }
+  }
+}).observe(container, {
+  subtree: true,
+  childList: true,
+});
